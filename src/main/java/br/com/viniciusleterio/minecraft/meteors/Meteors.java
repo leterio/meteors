b5e439d2eea3ffbe295e8d6fa82a22f68299c87c
Vinícius Letério <viniciusleterio@gmail.com>
2020-04-14 21:26:40 -0300

    Novo Projeto @ Criação do projeto "Meteors"

---
+++ src/main/java/br/com/viniciusleterio/minecraft/meteors/Meteors.java
@@ -0,0 +1,11 @@
+package br.com.viniciusleterio.minecraft.meteors;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Meteors.MODID, name = Meteors.NAME, acceptedMinecraftVersions = Meteors.ACCEPTED_VERSIONS, version = Meteors.VERSION)
+public class Meteors {
+	public static final String MODID = "meteors";
+	public static final String NAME = "${modname}";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String VERSION = "${version}";
+}
